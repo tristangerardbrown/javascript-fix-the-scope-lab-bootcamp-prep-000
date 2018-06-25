d4fe8cbc37e50b6eca90cde5d30be250d5f33dd2
Tristan Brown <tgbrown@stanford.edu>
2018-06-25 20:54:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,12 +7,9 @@ function yourAnimal() {
   var animal = 'cat'
   return animal 
 }
-
+const two = 2
 function add2(n) {
   return n + two
-
-  // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
